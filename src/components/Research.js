53feb607185bfe94cbc6f51b69551ae9bc86bb38
janahmedprg <jan.ahmed.prg@gmail.com>
2025-06-27 15:59:43 -0400

    trigger page build

--- src/components/Research.js
+++ src/components/Research.js
@@ -16,7 +16,7 @@ import lis3 from "../img/lis3.png";
 const Research = () => {
   const [selectedProj, setSelectedProj] = useState("graph");
   const handleClick = (event) => {
-    const { id } = event.currentTarget; // Get the id of the clicked div
+    const { id } = event.currentTarget;
     setSelectedProj(id); // Set the selectedProj state to the id
   };
 
